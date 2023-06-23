d9a655327e0beefeb10576a2f8cd75f76910b1e4
EfeCanEKEN <efecaneken06@gmail.com>
2023-06-23 18:48:15 +0300

    Son Kontrol
    23.06.2023

--- MvcEticaret/Controllers/HomeController.cs
+++ MvcEticaret/Controllers/HomeController.cs
@@ -11,13 +11,14 @@ namespace MvcEticaret.Controllers
         {
             var products = new List<Product>();
 
-            products.Add(new Product() { Title = "Ürün 1", IsSale = true, OldPrice = 30, StarCount = 3, ImageUrl = "", Price = 50 });
-            products.Add(new Product() { Title = "Ürün 2", IsSale = true, OldPrice = 30, StarCount = 3, ImageUrl = "", Price = 50 });
-            products.Add(new Product() { Title = "Ürün 3", IsSale = true, OldPrice = 30, StarCount = 3, ImageUrl = "", Price = 50 });
-            products.Add(new Product() { Title = "Ürün 4", IsSale = true, OldPrice = 30, StarCount = 3, ImageUrl = "", Price = 50 });
-            products.Add(new Product() { Title = "Ürün 5", IsSale = true, OldPrice = 30, StarCount = 3, ImageUrl = "", Price = 50 });
-            products.Add(new Product() { Title = "Ürün 6", IsSale = true, OldPrice = 30, StarCount = 3, ImageUrl = "", Price = 50 });
-            products.Add(new Product() { Title = "Ürün 7", IsSale = true, OldPrice = 30, StarCount = 3, ImageUrl = "", Price = 50 });
+            products.Add(new Product() { Title = "Altera FPGA Development Board", IsSale = true, OldPrice = 1.099M, StarCount = 4, ImageUrl = "/images/AlteraFPGADevelopmentBoard.jpg", Price = 999 });
+            products.Add(new Product() { Title = "Artix 7 FPGA Development Board", IsSale = true, OldPrice = 1.678M, StarCount = 5, ImageUrl = "/images/Artix7FPGADevelopmentBoard.jpg", Price = 1.550m });
+            products.Add(new Product() { Title = "Nexys A7-100T FPGA Development Board", IsSale = true, OldPrice = 265, StarCount = 3, ImageUrl = "/images/NexysA7-100T-FPGADevelopmentBoard.jpg", Price = 250 });
+            products.Add(new Product() { Title = "PIC Development Board", IsSale = false, OldPrice = 10, StarCount = 3, ImageUrl = "/images/PICDevelopmentBoard.jpg", Price = 30 });
+            products.Add(new Product() { Title = "Arduino Uno", IsSale = false, OldPrice = 30, StarCount = 4, ImageUrl = "/images/ArduinoUno.jpg", Price = 50 });
+            products.Add(new Product() { Title = "Raspberry Pi 4", IsSale = false, OldPrice = 30, StarCount = 5, ImageUrl = "/images/RaspberryPi4.jpg", Price = 50 });
+            products.Add(new Product() { Title = "DJI M30 Endüstriyel Drone", IsSale = true, OldPrice = 9.125M, StarCount = 4, ImageUrl = "/images/Drone.jpeg", Price = 8.799m });
+            products.Add(new Product() { Title = "RC Tank", IsSale = true, OldPrice = 1.099M, StarCount = 5, ImageUrl = "/images/RcTank.jpg", Price = 899m });
 
             return View(products);
         }
